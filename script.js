cf5439ca6c0c4ca909a0d635a7d62d11cae3c317
JacquieSue <112510142+JacquieSue@users.noreply.github.com>
2022-09-30 11:55:00 -0700

    wrote 'function' and 'for' statement

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
+// Array of options for computer to pick from
+var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+
 // Assignment code here
+function genPassword(passwordLength) {
+  var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var passwordLength = 20;
+  var password = " ";
 
+  for (var i = 0; i < passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber + 1)
+  }
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+  document.generatePassword("password").value = password;
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  // Get references to the #generate element
+  var generateBtn = document.querySelector("#generate");
+  var password = document.generatePassword("password");
+
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+
+  }
+
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
+
+
+alert(genPassword)(20);
