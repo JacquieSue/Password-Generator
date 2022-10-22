a8eae5cdec9f223abc973b6b55980b4ab88fac5f
JacquieSue <112510142+JacquieSue@users.noreply.github.com>
2022-10-22 08:04:52 -0700

    added rows, cards, and styling

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
+var generateBtn = document.querySelector("#generate");
+
 // Array of options for computer to pick from
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var passwordOptions = ["numbers", "lowerCase", "specialCharacters", "upperCase"];
+// var passwordOptions = ["numbers", "lowerCase", "specialCharacters", "upperCase"];
 var passwordLength = 20;
 var password = " ";
-
-//var generateBtn = document.querySelector("#generate");
-//var generatePassword = generatePassword();
 
 // Assignment code here
 var generatePassword = function () {
@@ -19,49 +18,53 @@ var generatePassword = function () {
     return;
   }
 
-  var special = window.prompt("Click okay to confirm including special characters.");
+  var special = window.confirm("Click okay to confirm including special characters.");
 
 
-  var userChoice = window.prompt("Click okay to confirm including numeric characters");
+  var numberic = window.confirm("Click okay to confirm including numeric characters");
 
 
-  var userChoice = window.prompt("Click okay to confirm including lowercase characters");
+  var lower = window.confirm("Click okay to confirm including lowercase characters");
 
 
-  var userChoice = window.prompt("Click okay to confirm including uppercase characters");
+  var upper = window.confirm("Click okay to confirm including uppercase characters");
 
-  if (!userChoice) {
-    return;
 
+  var options = []
+
+  if (special) {
+    options.push(...specialCharacters);
   }
 
-  // Write password to the #password input
-  //var passwordText = document.querySelector("#password");
-  //var passwordGen = document.generatePassword("password");
-  //var passwordOptions = options[numbers, lowerCase, specialCharacters, upperCase];
-
-  //if (userChoice) = yes
-  var passwordOptions = ["numbers", "lowerCase", "specialCharacters", "upperCase"];
-  var writePassword = function () {
-    for (var i = 0; i < passwordOptions.length; i++) {
-      var passwordOptions = Math.floor(Math.random() * passwordOptions.length);
-      password += options.substring(randomNumber, randomNumber + 1);
-      getAlphaNumericString(n);
-    }
-    // Add event listener to generate button
-    generateBtn.addEventListener("click", writePassword);
-    var generateBtn = document.querySelector("#generate");
-    var passwordText = document.querySelector("#password");
-
+  if (numberic) {
+    options.push(...numbers);
   }
-  //console.log(passwordOptions);
-  // Add event listener to generate button
-  //document.getElementById[password];
-  //document.generatePassword("password").value = password;
-  //passwordText.value = password;
-  //generateBtn.addEventListener("click", writePassword);
-  //alert(generatePassword)(20);
+  if (lower) {
+    options.push(...specialCharacters);
+  }
+
+  if (upper) {
+    options.push(...numbers);
+  }
+
+  // console.log(options)
+  // for (var i = 0; i < userChoice; i++) {
+  //   var passwordOptions = Math.floor(Math.random() * passwordOptions.length);
+  //   password += options.substring(randomNumber, randomNumber + 1);
+
+  // // }
+
+  return" ";
 }
 
-generatePassword();
 
+var writePassword = function () {
+  var password = generatePassword()
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password
+}
+
+
+
+generateBtn.addEventListener("click", writePassword);
+generatePassword();
